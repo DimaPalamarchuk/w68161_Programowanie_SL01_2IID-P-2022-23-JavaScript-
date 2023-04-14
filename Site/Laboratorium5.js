416a3c05880a9d896a4c641ce1becda84c1c4b4c
DimaPalamarchuk <dimapalamarchuk310@gmail.com>
2023-04-14 14:37:30 +0200

    Laboratorium

--- Site/Laboratorium5.js
+++ Site/Laboratorium5.js
@@ -1,4 +1,8 @@
 // Zadanie 1
+
+//Napisz program, który poprosi użytkownika o podanie swojego wieku. Zapisz to
+//do zmiennej i sprawdź, czy jest pełnoletni. Wypisz odpowiedni komunikatu na ekranie.
+
 let wiek = prompt('Podaj swój wiek: ');
 if (wiek >=18) {
     console.log('Jesteś pełnoletni!');
@@ -9,6 +13,10 @@ if (wiek >=18) {
 
 
 // Zadanie 2
+
+//Dokonaj konwersji stopni Celsjusza na stopnie Fahrenheita na podstawie podanej temperatury.
+//Wypisz wartość konsoli.
+
 let stopnieC = prompt('Podaj stopnieC: ');
 let stopnieF = (stopnieC * 9/5) + 32;
 console.log('Temperatura w stopniach Fahrenheita:' + stopnieF);
@@ -17,34 +25,30 @@ console.log('Temperatura w stopniach Fahrenheita:' + stopnieF);
 
 // Zadanie 3
 
+//Stwórz tablice liczb i wykonaj na niej poniższe operacje
+//Do operacji wykorzystaj metody tablic. Wynik operacji wypisz w konsoli.
+
 let numbersArray = [2, 5, 8, 10, 12, 15, 20, 25, 30, 35, 40, 45, 50];
 
-// Zsumuj wartości
 let sum = numbersArray.reduce(function(acc, val) { return acc + val; }, 0);
 console.log("Suma wartości: " + sum);
 
-// Znajdź liczby parzyste
 let evenNumbers = numbersArray.filter(function(num) { return num % 2 === 0; });
 console.log("Liczby parzyste: " + evenNumbers);
 
-// Pomnóż wartości razy 3
 let multipliedArray = numbersArray.map(function(num) { return num * 3; });
 console.log("Pomnożone wartości: " + multipliedArray);
 
-// Dodaj do tablicy swój numer albumu. Znajdź jego index w tablicy
 numbersArray.push(12345);
 let albumIndex = numbersArray.indexOf(12345);
 console.log("Index mojego numeru albumu: " + albumIndex);
 
-// Oblicz średnią arytmetyczna
 let avg = sum / numbersArray.length;
 console.log("Średnia arytmetyczna: " + avg);
 
-// Znajdź największą liczbę
 let max = Math.max(...numbersArray);
 console.log("Największa liczba: " + max);
 
-// Zlicz ilość wystąpień wybranej wartości
 let valueToCount = 25;
 let count = numbersArray.filter(function(num) { return num === valueToCount; }).length;
 console.log("Liczba wystąpień wartości " + valueToCount + ": " + count);
@@ -52,6 +56,9 @@ console.log("Liczba wystąpień wartości " + valueToCount + ": " + count);
 
 
 // Zadanie 4
+
+//Stwórz tablice z 100 elementami zawierający kolejnymi liczbami ciągu Fibonacciego.
+
 let fibonacciArray = [0,1];
 for (let i = 2; i < 100; i++) {
     fibonacciArray[i] = fibonacciArray[i-1]+fibonacciArray[i-2];
